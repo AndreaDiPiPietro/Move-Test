e3abd576bf1aaa2f6cb0ce20cd9c2fbcbb752fca
Ninechi <dipietroandrea@hotmail.it>
2018-11-23 02:28:02 +0100

    Initial Commit

---
+++ Assets/Scripts/PlaymodeEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaymodeEditor : MonoBehaviour {
+
+    public GameObject anchor;
+    public float spacing = .1f;
+    public float resolution = 1;
+    PathCreator creator;
+
+    private void Start()
+    {
+        creator = FindObjectOfType<PathCreator>();
+    }
+
+    public void DrawAnchors()
+    {
+        for (int i = 0; i < creator.path.NumPoints; i++)
+        {
+            Instantiate(anchor, creator.path.points[i], Quaternion.identity, this.gameObject.transform);
+        }
+    }
+
+}
---
+++ Assets/Scripts/RoadSpawn.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoadSpawn : MonoBehaviour {
+
+    public GameObject anchorPoint;
+    public GameObject ducetto;
+    public Button roadButton;
+    List<GameObject> anchorList;
+    LineRenderer lineRenderer;
+    PathCreator pathCreator;
+
+    public void cleanList()
+    {
+        lineRenderer.positionCount = 0;
+        anchorList.Clear();
+    }
+    private void Start()
+    {
+        anchorList = new List<GameObject>();
+        lineRenderer = GetComponent<LineRenderer>();
+        pathCreator = GetComponent<PathCreator>();          
+    }
+
+    public void SpawnAnchor()
+    {
+        lineRenderer.positionCount += 1;
+        var cur = Instantiate(anchorPoint,transform);
+        anchorList.Add(cur);
+
+        if (anchorList.Count == 1)
+            StartCoroutine(FirstSpawn());
+        else if (anchorList.Count > 1)
+        {
+            Debug.Log("Mo");
+            pathCreator.path.AddSegment(cur.transform.position);
+        }
+    }
+
+    IEnumerator FirstSpawn()
+    {
+        if (transform.GetChild(0).GetComponent<DragMovement>().IsDragging)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        else
+        {
+            pathCreator.CreatePath(anchorList[0].transform.position);
+            //SpawnAnchor();
+            yield return null;
+        }
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetMouseButtonDown(0))
+        {
+            SpawnAnchor();
+        }
+
+        for (int j = 0 ;  j < anchorList.Count; j++)
+        {
+            Vector3 adjustedPos = new Vector3(anchorList[j].transform.position.x, 0.3f, anchorList[j].transform.position.z);
+            lineRenderer.SetPosition(j, adjustedPos);
+            pathCreator.path.MovePoint(j*3, adjustedPos);
+        }
+    }
+}
